acb4d01f06be8e22a89779199c6da6e10e5969a5
SoumyadityaDutta-rgb <soumyadityadutta40@gmail.com>
2025-10-19 13:59:29 +0530

    Delete helpers.js

--- helpers.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken");
-exports = {}
-exports.getToken = async (email, user) => {
-    const token = jwt.sign({ id: user._id }, "myworldmywish", { expiresIn: "1h" });
-
-    return token;
-};
-module.exports = exports;
